ec12dd2f87b3f1c17746f04c35aa0e0cde488153
Slewis916 <113942694+Slewis916@users.noreply.github.com>
2024-05-29 14:16:35 -0400

    test darktheme

--- _js/src/flip/index.js
+++ _js/src/flip/index.js
@@ -25,3 +25,21 @@ export function setupFLIP(start$, ready$, fadeIn$, options) {
 
   return merge(setupFLIPTitle(start$, ready$, fadeIn$, options), other$);
 }
+
+
+const btn = document.querySelector(".btn-toggle");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
