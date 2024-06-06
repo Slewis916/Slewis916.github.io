425d28d49684d0513c7e53f742ea8ffc1e44d9a5
Slewis916 <113942694+Slewis916@users.noreply.github.com>
2024-06-06 04:56:04 -0400

    TEST

---
+++ assets/DynamicResume.vue
@@ -0,0 +1,16 @@
+<template>
+ <div id="DynamicResume">
+    <h1>{{ metadata.basics.name }}</h1>
+ </div> 
+</template>
+
+<script>
+export default {
+    name: 'DynamicResume',
+    props: ['metaData'],
+    created() {
+        console.log("Exported Module");
+    }
+}
+
+</script>
